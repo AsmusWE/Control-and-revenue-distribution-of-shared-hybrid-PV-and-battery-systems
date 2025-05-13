ada73f7e2bf103f3d8a9ad774d3efbf4ed174e93
AsmusWE <146201076+AsmusWE@users.noreply.github.com>
2025-05-13 16:59:20 +0200

    Changed imbalance to be calculated for a single day at a time, and enabled multithreading for solving coalitions

--- Imbalance_calc.jl
+++ Imbalance_calc.jl
@@ -3,7 +3,7 @@ include("Common_functions.jl")
 include("Data_import.jl")
 include("Scenario_creation.jl")
 include("imbalance_reduction.jl")
-using Plots, Dates, Random
+using Plots, Dates, Random, Combinatorics
 
 function daily_imbalance(bids, pvProd, demand)
     pvProd = pvProd[1:length(bids)] # Ensure pvProd is the same length as bids
@@ -12,46 +12,73 @@ function daily_imbalance(bids, pvProd, demand)
     return imbalance
 end
 
-function calculate_imbalance(systemData, clients; days = 5, plotting = false)
-    bids = zeros(24, days)
-    demand_sum = zeros(24, days)
-    imbalance = zeros(24, days)
-    scaled_pvProd = zeros(24, days)
-    for day in 1:days
-        tempData = deepcopy(systemData)
-        tempData["price_prod_demand_df"] = systemData["price_prod_demand_df"][(day-1)*24+1:day*24, :]
-        bids[:,day] = optimize_imbalance(clients, tempData)
-        demand_sum[:,day] = sum(tempData["price_prod_demand_df"][!, i] for i in clients)
-        scaled_pvProd[:,day] = tempData["price_prod_demand_df"][!, "SolarMWh"] .* sum(tempData["clientPVOwnership"][i] for i in clients)
-        imbalance[:,day] = daily_imbalance(bids[:,day], scaled_pvProd[:,day], demand_sum[:,day])
+function calculate_imbalance(systemData, clients; plotting = false)
+    coalitions = collect(combinations(clients))
+    @time begin
+    bids = calculate_bids(coalitions, systemData)
     end
-
+    demand_sum = Dict()
+    scaled_pvProd = Dict()
+    imbalance = Dict()
+    for coalition in coalitions
+        demand_sum[coalition] = sum(systemData["price_prod_demand_df"][!, i] for i in coalition)
+        scaled_pvProd[coalition] = systemData["price_prod_demand_df"][!, "SolarMWh"] .* sum(systemData["clientPVOwnership"][i] for i in coalition)
+        imbalance[coalition] = daily_imbalance(bids[coalition], scaled_pvProd[coalition], demand_sum[coalition])
+    end
+    
     if plotting
         avg_imbalance = sum(imbalance, dims=2) / size(imbalance, 2)
         avg_demand = sum(demand_sum, dims=2) / size(demand_sum, 2)
         avg_production = sum(scaled_pvProd, dims=2) / size(scaled_pvProd, 2)
         avg_bids = sum(bids, dims=2) / size(bids, 2)
-        avg_demand_scenarios = sum(sum(demand_scenarios, dims=3), dims=2) / size(demand_scenarios, 3)
+        demand_scens_sum = sum(systemData["demand_scenarios"][c] for c in clients)
+        avg_demand_scenarios = sum(demand_scens_sum, dims=2) / size(demand_scens_sum, 2)
 
-        plot(1:24, avg_imbalance, label="Average Imbalance", xlabel="Hour", ylabel="Value", title="Average Values Over 24 Hours")
-        plot!(1:24, avg_demand, label="Average Demand")
-        plot!(1:24, avg_production, label="Average Production")
-        plot!(1:24, avg_bids, label="Average Bids")
-        plot!(1:24, avg_demand_scenarios[:], label="Average Demand Scenarios")
+        p = plot(1:24, avg_imbalance, label="Average Imbalance", xlabel="Hour", ylabel="Value", title="Average Values Over 24 Hours")
+        plot!(p, 1:24, avg_demand, label="Average Demand")
+        plot!(p, 1:24, avg_production, label="Average Production")
+        plot!(p, 1:24, avg_bids, label="Average Bids")
+        plot!(p, 1:24, avg_demand_scenarios[:], label="Average Demand Scenarios")
+        
+        display(p)
     end
 
-    total_imbalance = sum(imbalance)
-    return total_imbalance
+    return imbalance, bids
 end
 
-Random.seed!(1234) # Set seed for reproducibility
+function calculate_bids(coalitions, systemData)
+    # This function calculates the bids for each coalition combination
+    bids = Dict()
+    Threads.@threads for clients in coalitions
+        bids[clients] = optimize_imbalance(clients,systemData)
+    end
+    return bids
+end
+
+Random.seed!(12) # Set seed for reproducibility
 
 systemData, clients_without_missing_data = load_data()
 clients_without_missing_data = filter(x -> x != "Z", clients_without_missing_data)
-demand_scenarios = generate_scenarios(clients_without_missing_data, systemData["price_prod_demand_df"]; num_scenarios=100)
+clients_without_missing_data = filter(x -> !(x in ["W", "T", "P", "V", "J", "F","R","K","U","Y","O","A"]), clients_without_missing_data)
+
+coalitions = collect(combinations(clients_without_missing_data))
+demand_scenarios = generate_scenarios(clients_without_missing_data, systemData["price_prod_demand_df"]; num_scenarios=200)
 systemData["demand_scenarios"] = demand_scenarios
 
+simulation_days = 1
 
-imbalance = calculate_imbalance(systemData, clients_without_missing_data; days=5)
+imbalance_results = Dict()
 
+dayData = deepcopy(systemData)
+dayData["price_prod_demand_df"] = systemData["price_prod_demand_df"][1:24, :]
+imbalances, bids = calculate_imbalance(dayData, clients_without_missing_data; plotting=false)
+println("Test")
+#total_imbalance, bids = calculate_imbalance(systemData, clients_without_missing_data; days=simulation_days, plotting=false)
 
+#for (idx,clients) in enumerate(client_combinations)
+#    if idx % 1000 == 0
+#        println("Processing combination: ", idx, " of ", length(client_combinations))
+#    end
+#    total_imbalance, bids = calculate_imbalance(systemData, clients; days=simulation_days, plotting=false)
+#    imbalance_results[clients] = total_imbalance
+#end
--- Scenario_creation.jl
+++ Scenario_creation.jl
@@ -1,19 +1,19 @@
-
-
 function generate_scenarios(clients, demandDF; num_scenarios = 100, scen_length = 24)
-    # Create an array to store the scenarios
-    
-    total_scenarios = div(size(demandDF)[1], scen_length, RoundDown) 
-    scenarios = zeros(scen_length, length(clients), total_scenarios)
-    for i in 1:total_scenarios
-        # Generate a random scenario for each client
-        for (idx,j) in enumerate(clients)
-            # Setting each scenario to be a demand sample
-            scenarios[:, idx, i] = demandDF[!, j][(i-1)*scen_length+1:i*scen_length] 
+    # Create a dictionary to store the scenarios
+    total_scenarios = div(size(demandDF)[1], scen_length, RoundDown)
+    scenarios_dict = Dict()
+
+    for client in clients
+        # Initialize an array to store scenarios for the client
+        client_scenarios = zeros(scen_length, total_scenarios)
+        for i in 1:total_scenarios
+            # Setting each scenario to be a demand sample for the client
+            client_scenarios[:, i] = demandDF[!, client][(i-1)*scen_length+1:i*scen_length]
         end
+        # Randomly select num_scenarios from the generated scenarios
+        selected_indices = rand(1:total_scenarios, num_scenarios)
+        scenarios_dict[client] = client_scenarios[:, selected_indices]
     end
-    # Randomly select num_scenarios from the generated scenarios
-    selected_indices = rand(1:total_scenarios, num_scenarios)
-    scenarios = scenarios[:, :, selected_indices]
-    return scenarios
+
+    return scenarios_dict
 end
